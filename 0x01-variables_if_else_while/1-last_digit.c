73183a1f9052cbc1735bd83986e3a1f34f04eedb
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-01 16:50:30 -0700

    Push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,8 +27,9 @@ else if (d == 0)
 {
 	printf("Last Digit of %d is %d and is 0\n", n, d);
 }
-else (d < 6 && d != 0)
+else(d < 6 && d != 0)
 {
+
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 }
 
