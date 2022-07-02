65c375f996047a0b9e4e480929cfb04049f2220e
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-02 01:04:08 -0700

    lower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main-Start the code
- * return:always 0
- */
+*main-Start the code
+*return:always 0
+*/
 int main()
 {
 int ch;
 for(ch = 97; ch <= 122; ch++)
 {
 	putchar(ch);
-	putchar('\n');
+	
 }
 return (0);
 }
