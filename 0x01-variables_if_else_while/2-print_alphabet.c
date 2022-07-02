ac785ee0a5786ce811d55b8ef529865e8d280b2e
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-01 17:27:59 -0700

    Aa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main- Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+
+intx
+for (x = 'A'; x >= 'Z'; x++)
+{
+	putchar(tolower(x));
+	putchar('\n');
+}
+return (0);
+}
