693ae10e1cb5b331e2cb13d67ddc9d8d0df84c0e
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-02 02:56:42 -0700

    Uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /**
-*main- entry point
-*return: always 0
+*main- entry point, prints lowercase followed by uppercase
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-int ch;
-for (ch = 97; ch <= 122; ch++)
-{
-	putchar(ch);
-}
-for (ch = 65; ch <= 90; ch++)
-{
-	putchar(ch);
-}
-return (0);
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0);
 }
