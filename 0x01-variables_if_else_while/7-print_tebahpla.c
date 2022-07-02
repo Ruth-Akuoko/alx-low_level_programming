df506548fc68170f232077b02933bde197eb4345
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-02 05:07:58 -0700

    FLDUK

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry Point
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 122; x <= 97; x--)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
