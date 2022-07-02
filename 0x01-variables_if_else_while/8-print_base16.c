d4d7789c840e6d888676c7c4221f83851cf02561
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-02 05:37:04 -0700

    opp

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d >= 102; d++)
+	{
+		putchar(d);
+	}
+	 putchar('\n');
+	return (0);
+}
