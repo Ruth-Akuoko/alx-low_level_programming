bd402454c404932594487cbb595ba357a1950e7b
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-03 16:06:04 -0700

    combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int thousands;
+
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (tens = ones + '1'; tens <= '8'; tens++)
+			{
+				for (thousands = '2'; thousands <= '9'; thousands++)
+				{
+					putchar(ones);
+					putchar(tens);
+					putchar(thousands);
+					if (ones != 7 || tens != '8' || thousands != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+						putchar('\n');
+		}
+
+						return (0);
+}
+
