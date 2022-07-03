bff1a9d0f741372a6c4c8089d9b143186a25d1c0
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-03 15:04:29 -0700

    two

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,20 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = 0; tens <= 9; tens++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = tens + 1; ones <= 9; ones++)
+		for (ones = tens + 1; ones <= '9'; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
-			if (tens != 8 || ones != 9)
+			if (tens != '8' || ones != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
 
