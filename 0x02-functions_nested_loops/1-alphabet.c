a4ad44c0cbc76ae6a8e065c4f609a66a8479115a
Ruth-Akuoko <akuokoruth1@gmail.com>
2022-07-06 05:46:13 -0700

    lop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet- Prints alpahabets
+ *Return: Returns 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x >= 'b'; x++)
+	{
+		_putchar(x);
+	}
+		_putchar('/n');
+		return (0);
+}
